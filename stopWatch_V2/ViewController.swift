3d34fffbc199a968d5792a0d7ccab873dc2febb8
Magnus Kruschwitz <magnus.kruschwitz@outlook.com>
2020-01-10 21:19:35 +0100

    Initial Commit

---
+++ stopWatch_V2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  stopWatch_V2
+//
+//  Created by Magnus Kruschwitz on 10.01.20.
+//  Copyright © 2020 Magnus Kruschwitz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
